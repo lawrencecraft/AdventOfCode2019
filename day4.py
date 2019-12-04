fc34c21b9336259aa7c47c7ea34ef7cb05422ac8
Lawrence Craft <lawrence.craft@viavisolutions.com>
2019-12-04 11:17:48 +0000

    Day4 pt2

--- day4.py
+++ day4.py
@@ -1,16 +1,21 @@
 def testNum(n):
     last = None
+    currentStreak = 1
     hasDubbadub = False
     while n > 0:
         current = n % 10
         if last is not None:
             if last < current:
                 return False
-            if not hasDubbadub and last == current:
-                hasDubbadub = True
+            if last == current:
+                currentStreak += 1
+            else:
+                hasDubbadub = hasDubbadub or currentStreak == 2
+                currentStreak = 1
+
         last = current
         n = n // 10
-    return hasDubbadub
+    return hasDubbadub or currentStreak == 2
 
 if __name__ == "__main__":
     print(sum(1 for i in range(357253, 892942) if testNum(i)))
