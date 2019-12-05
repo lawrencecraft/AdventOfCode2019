afc89089f2397e894518e4e2dd7fbdbfcd1e46ee
Lawrence Craft <lawrence.craft@gmail.com>
2019-12-05 22:50:21 +0000

    Day 5 part 1

--- day5.py
+++ day5.py
@@ -1,52 +1,102 @@
+import sys
+
+from typing import NamedTuple
+from typing import List
 
 OPCODE_ADD = 1
 OPCODE_MUL = 2
+OPCODE_INPUT = 3
+OPCODE_OUTPUT = 4
 OPCODE_HCF = 99
 
-def executeInstruction(index, programSpace, transform):
+MODE_DIRECT = 1
+MODE_POSITION = 0
+
+class Instruction(NamedTuple):
+    opcode: int
+    arg1_mode: int
+    arg2_mode: int
+    arg3_mode: int
+
+class ProgramResult(NamedTuple):
+    ram: List[int]
+    output: List[int]
+
+def resolve(value, mode, programSpace):
+    if mode == MODE_DIRECT:
+        return value
+    elif mode == MODE_POSITION:
+        return programSpace[value]
+    else:
+        raise Exception(f"Unknown mode: {mode}")
+
+
+def parseInstruction(val) -> Instruction:
+    code = val % 100
+    modes = val // 100
+    mode1 = modes % 10
+    mode2 = (modes // 10) % 10
+    mode3 = (modes // 100) % 10
+
+    return Instruction(opcode = code, arg1_mode = mode1, arg2_mode = mode2, arg3_mode = mode3)
+
+
+def executeInstruction(index, instruction, programSpace, transform):
     src1, src2, dst = programSpace[index + 1:index + 4]
-    programSpace[dst] = transform(programSpace[src1], programSpace[src2])
+    val1 = resolve(src1, instruction.arg1_mode, programSpace)
+    val2 = resolve(src2, instruction.arg2_mode, programSpace)
+    programSpace[dst] = transform(val1, val2)
+
 
 
 def processInstruction(index, programSpace) -> bool:
-    opcode = programSpace[index]
-    if opcode == OPCODE_ADD:
-        executeInstruction(index, programSpace, lambda x, y: x + y)
-        return False
-    elif opcode == OPCODE_MUL:
-        executeInstruction(index, programSpace, lambda x, y: x * y)
-        return False
-    elif opcode == OPCODE_HCF:
-        return True
+    rawValue = programSpace[index]
+    instruction = parseInstruction(rawValue)
+    if instruction.opcode == OPCODE_ADD:
+        executeInstruction(index, instruction, programSpace, lambda x, y: x + y)
+        return 4
+    elif instruction.opcode == OPCODE_MUL:
+        executeInstruction(index, instruction, programSpace, lambda x, y: x * y)
+        return 4
+    elif instruction.opcode == OPCODE_OUTPUT:
+        val = programSpace[index + 1]
+        print(resolve(val, instruction.arg1_mode, programSpace))
+
+        return 2
+        
+    elif instruction.opcode == OPCODE_INPUT:
+        val = int(sys.stdin.readline().strip())
+        dest = programSpace[index + 1]
+        programSpace[dest] = val
+
+        return 2
+    elif instruction.opcode == OPCODE_HCF:
+        return 0
     else:
-        raise Exception(f"Unknown opcode: {opcode}")
+        raise Exception(f"Unparseable opcode: {rawValue}")
 
 
-
-def runProgram(initialRam, input1, input2) -> int:
+def runProgram(initialRam) -> ProgramResult:
     programSpace = initialRam.copy()
-    programSpace[1] = input1
-    programSpace[2] = input2
+    instructionPointer = 0
 
-    for currentInstruction in range(0, len(programSpace), 4):
-        if processInstruction(currentInstruction, programSpace):
+    programSize = len(programSpace)
+
+    while instructionPointer < programSize:
+        pointerMovement = processInstruction(instructionPointer, programSpace)
+
+        if not pointerMovement:
             break
-    return programSpace[0]
+
+        instructionPointer += pointerMovement
 
 
 def doIt():
     ops = []
-    with open("input") as f:
+    with open("input_day5") as f:
         ops = list(map(int, f.read().split(',')))
 
-    for input1 in range(0, 100):
-        for input2 in range(0, 100):
-            if runProgram(ops, input1, input2) == 19690720:
-                print(f"Inputs: {input1}, {input2}")
-                print(f"{input1 * 100 + input2}")
-    
-
-
+    runProgram(ops)
 
 if __name__ == "__main__":
     doIt()
