96e5c804f4eb689db662b875d2bd71aebe31c52b
Lawrence Craft <lawrence.craft@gmail.com>
2019-12-06 23:18:10 +0000

    day6 part1

--- day6.py
+++ day6.py
@@ -1,11 +1,30 @@
+from typing import List
+from typing import Tuple
 
-def loadOrbits():
+from collections import defaultdict
+
+def loadOrbits() -> List[Tuple[str, str]]:
     with open("input_day6") as f:
-        return [o.split(')') for o in f.readlines()] 
+        return [tuple(o.strip().split(')')) for o in f.readlines()] 
 
-def solveIt(orbits):
-    print(len(orbits))
+
+def solveIt(orbits: List[Tuple[str, str]]):
+    childToParent = {}
+    for p, c in orbits:
+        childToParent[c] = p
+
+    memoized_orbits = {}
+    def countOrbits(c):
+        if c in memoized_orbits:
+            return memoized_orbits[c]
+        elif c not in childToParent:
+            return 0
+        else:
+            orbs = 1 + countOrbits(childToParent[c])
+            memoized_orbits[c] = orbs
+            return orbs
+    return sum(countOrbits(c) for c in childToParent)
 
 if __name__ == "__main__":
     orbits = loadOrbits()
-    solveIt(orbits)
+    print(solveIt(orbits))
