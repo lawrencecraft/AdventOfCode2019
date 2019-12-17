3b7a035eb22bd39a8f26c3a4571596393c0b1868
Lawrence Craft <lawrence.craft@viavisolutions.com>
2019-12-17 01:43:32 +0000

    day12 pt1

---
+++ day12.py
@@ -0,0 +1,52 @@
+def calculateGravity(p1, p2):
+    def calc(x):
+        i,j = x
+        if i == j:
+            return 0
+        elif i > j:
+            return -1
+        else:
+            return 1
+    return tuple(map(calc, zip(p1, p2)))
+
+def calculatePlanetGravities(planets):
+    velocityChanges = [(0, 0, 0) for _ in planets]
+    for i1, p1 in enumerate(planets):
+        for basei2, p2 in enumerate(planets[i1+1:]):
+            i2 = basei2 + i1 + 1
+            gravity = calculateGravity(p1, p2)
+            def app(i, v):
+                velocityChanges[i] = tuple(map(sum, zip(velocityChanges[i], v)))
+            app(i1, gravity)
+            app(i2, map(lambda x: -x, gravity))
+
+    return velocityChanges
+
+def energy(p, v):
+    potential = sum(map(abs, p))
+    kinetic = sum(map(abs, v))
+    return potential * kinetic
+
+def runSimulation(startingPositions, iterations = 1000):
+    positions = startingPositions
+    velocities = [(0, 0, 0) for _ in startingPositions]
+
+    def addLists(l1, l2):
+        return [tuple(map(sum, zip(*vs))) for vs in zip(l1, l2)]
+    
+    for step in range(iterations):
+        # Calculate gravity
+        velocityChanges = calculatePlanetGravities(positions)
+        velocities = addLists(velocityChanges, velocities)
+        # Update positions
+        positions = addLists(positions, velocities)
+        
+    e = sum(energy(p, v) for p, v in zip(positions, velocities))
+    print(f"Energy: {e}")
+
+if __name__ == "__main__":
+    positions = []
+    with open("input_day12") as f:
+        for l in f.readlines():
+            positions.append(tuple(map(int, l.split(','))))
+    runSimulation(positions)
